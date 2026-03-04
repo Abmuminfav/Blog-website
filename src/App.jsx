461235262a55c4e93848532ac1cbf35eb176b7f6
Abdulmumin <abdulmumin@mailbox.unideb.hu>
2026-03-04 02:37:10 -1200

    create post not showing

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/Createpost" element={<Createpost/>}/>
+        <Route path="/Create" element={<Createpost/>}/>
       </Routes>
     </>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ function Navbar(){
         <h1 className="font-bold text-4xl">BlockTalk</h1>
         <ul className="hidden md:flex gap-6 items-center">
              <li><Link to="/">Home</Link></li>
-             <li><Link to="/CreatePost">Create Post</Link></li>
+             <li><Link to="/Create">Create Post</Link></li>
              <li><Link to="/About">About</Link></li>
              <li><Link to="/Donate">
                 <button className="border-[#FB015A] p-2 rounded-sm bg-black text-white">Donate Now</button>
@@ -22,7 +22,7 @@ function Navbar(){
         <div className={`md:hidden absolute top-full left-0 w-full bg-black/90 backdrop-blur-lg shadow-md transition-all duration-300 overflow-hidden ${open ? "max-h-96" : "max-h-0"}`}>
             <ul className="flex flex-col gap-6 text-white p-2">
                 <li onClick={() => setOpen(false)}><Link to="/">Home</Link></li>
-                <li onClick={() => setOpen(false)}><Link to="/Createpost">Create Post</Link></li>
+                <li onClick={() => setOpen(false)}><Link to="/Create">Create Post</Link></li>
                 <li onClick={() => setOpen(false)}><Link to="/About">About</Link></li>
                 <li onClick={() => setOpen(false)}><Link to="/Donate">
                 <button className="border-[#FB015A] p-2 rounded-sm bg-gray-500 text-white">Donate Now</button>
